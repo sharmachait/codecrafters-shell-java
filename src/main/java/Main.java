06a37961b744e1eba16be7018f9231fc50cffee3
sharmachait <chait8126@gmail.com>
2025-06-04 16:04:43 +0530

    #ff0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        while(true){
+            handleCommand();
+        }
+    }
+    public static void handleCommand(){
         System.out.print("$ ");
 
         Scanner scanner = new Scanner(System.in);
